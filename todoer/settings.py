c3bcbf684f1b35c6b62cf8243742f56de14e0722
Edchel Stephen <edchelstephens@gmail.com>
2021-07-15 21:43:27 +0800

    add database url.

--- todoer/settings.py
+++ todoer/settings.py
@@ -88,10 +88,10 @@ WSGI_APPLICATION = 'todoer.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(
+        "DATABASE_URL",
+        default="postgres:///todoer"
+    )
 }
 
 
