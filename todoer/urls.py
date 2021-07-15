43c595bd564e3de75d30a9d82cd21d8909a4cb82
Edchel Stephen <edchelstephens@gmail.com>
2021-07-16 07:53:25 +0800

    first step on apis, create viewsets.

--- todoer/urls.py
+++ todoer/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
+from api import urls
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include(urls.urlpatterns))
 ]
