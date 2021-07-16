f166f709dea122e1da20c6feef4cc7a1bcbfc52d
Edchel Stephen <edchelstephens@gmail.com>
2021-07-16 08:02:55 +0800

    ulrs for default api router.

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,11 @@ from rest_framework import routers
 from django.urls import path, include
 
 from api.serializers.user import UserViewSet
+from api.serializers.todo import TodoViewSet
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
+router.register(r"todos", TodoViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
