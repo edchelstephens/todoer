531aaa7b47fdd66a648ffe7e01f8839a73f3155d
Edchel Stephen <edchelstephens@gmail.com>
2021-07-16 08:17:37 +0800

    set default permission classes to IsAuthenticated.

--- config/settings/base.py
+++ config/settings/base.py
@@ -130,7 +130,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ]
 }
 
