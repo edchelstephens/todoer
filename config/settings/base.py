345c732664da3cd9c12907b5dee62d6966836a8b
Edchel Stephen <edchelstephens@gmail.com>
2021-07-16 08:28:05 +0800

    add TokenAuthentication.

--- config/settings/base.py
+++ config/settings/base.py
@@ -56,6 +56,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 PROJECT_APPS = [
@@ -129,6 +130,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES":[
+        "rest_framework.authentication.TokenAuthentication"
+    ],
+
     "DEFAULT_PERMISSION_CLASSES": [
         'rest_framework.permissions.IsAuthenticated'
     ]
